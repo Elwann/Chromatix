2db497deebc7dcf8f9a934e7b7d9cf95959f49b5
Loïc Huck <contact@huckloic.fr>
2015-04-11 17:07:26 +0200

    Movement setup

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Chromatix/Player/PlayerMovement")]
+public class PlayerMovement : MonoBehaviour {
+
+	Vector2 velocity;
+
+	//public Transform model;
+
+	private PlayerInput controller;
+	private Rigidbody2D rigidbody2D;
+	public BoxCollider2D collider2D;
+	//public PlayerGroundDetection playerDetection;
+	
+	public float maxSpeed = 20f;
+	public float normalAccel = 1f;
+	public float gravity = -9.81f;
+	int groundLayer;
+	//public float turnMul = 2f;
+	
+	public float initialJumpSpeed = 50f;
+	bool groundedLastFrame = false;
+
+	bool pressed = false;
+
+
+	void Awake()
+	{
+		groundLayer = LayerMask.NameToLayer("Ground");
+		collider2D = gameObject.GetComponent<BoxCollider2D>();
+		controller = gameObject.GetComponent<PlayerInput>();
+		rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update()
+	{
+		if(IsGrounded()){
+			GroundMove();
+		} else {
+			AirMove();
+		}
+
+		velocity.y += gravity;
+		velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = new Vector2(velocity.x, velocity.y);
+	}
+
+	void GroundMove()
+	{
+		velocity.y = 0;
+
+		float h = controller.Horizontal();
+		if(h != 0){
+			transform.localScale = new Vector3(Mathf.Sign(h), 1f ,1f);
+			velocity += new Vector2(h, 0f) * normalAccel;
+		} else {
+			velocity = Vector2.zero;
+		}
+
+		// Start jump from ground
+		if (controller.IsJumpPressed()) {
+			//Debug.Log(controller.IsJumpDown());
+			velocity.y = initialJumpSpeed;
+			return;
+		} else {
+			velocity.y = 0;
+		}
+	}
+
+	bool IsGrounded()
+	{
+		Vector2 start = new Vector2(collider2D.bounds.min.x, collider2D.bounds.min.y+0.05f);
+		Vector2 stop = new Vector2(collider2D.bounds.max.x, collider2D.bounds.min.y-0.05f);
+		Debug.DrawLine(start, stop, Color.green);
+		return Physics2D.OverlapArea(stop, start, 1 << groundLayer);
+
+		/*if(Mathf.Abs ( rigidbody2D.velocity.y ) < 0.1f) {	// Checking floats for exact equality is bad. Also good for platforms that are moving down.
+			
+			// Since it's possible for our velocity to be exactly zero at the apex of our jump,
+			// we actually require two zero velocity frames in a row.
+			
+			if(groundedLastFrame)
+				return true;
+			
+			groundedLastFrame = true;
+		} else {
+			groundedLastFrame = false;
+		}
+		
+		return false;*/
+	}
+
+	void AirMove()
+	{
+		if ((velocity.y > 0) && (!controller.IsJumpPressed()))
+			velocity.y = 0;
+
+		float h = controller.Horizontal();
+		transform.localScale = new Vector3(Mathf.Sign(h), 1f ,1f);
+		velocity += new Vector2(h, 0f) * normalAccel;
+	}
+
+	/*
+	
+	float MaxHorizontalSpeed() {
+		if (playerInput.IsBoostPressed()) return boostMaxHorizSpeed;
+		return normMaxHorizSpeed;
+	}
+	
+	float walkAccel() {
+		if (playerInput.IsBoostPressed()) return boostAccel;
+		return normalAccel;
+	}
+	
+	bool walk() {
+		float sign = 0;
+		
+		if (playerInput.IsRightPressed()) sign = 1;
+		else if (playerInput.IsLeftPressed()) sign = -1;
+		else return false;
+		
+		float currSign = Mathf.Sign(velocity.x);
+		float v = walkAccel();
+		if ((currSign != 0) && (currSign != sign))
+			v *= turnMul;
+		velocity.x += v * sign * Time.deltaTime * 60;
+		return true;
+	}
+	
+	void airControls() {
+		// Abort jump if user lets go of button 
+		if ((velocity.y > 0) && (!playerInput.IsJumpPressed()))
+			velocity.y = 0;
+		
+		// Air walk
+		walk();
+	}
+	
+	void groundControls() {
+		velocity.y = 0;
+		
+		// Start jump from ground
+		if (playerInput.IsJumpPressed()) {
+			velocity.y = initialJumpSpeed;
+			return;
+		} else {
+			velocity.y = 0;
+		}
+		
+		// Run on ground 
+		if (!walk())
+			velocity.x = 0;
+	}*/
+}
