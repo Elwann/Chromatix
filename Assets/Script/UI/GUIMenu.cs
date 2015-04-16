713fa94c06fd5349ba0aa88871525c5e379d23c5
Loïc Huck <loic@hotmail.fr>
2015-04-16 02:06:36 +0200

    Add sound

---
+++ Assets/Script/UI/GUIMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMenu : MonoBehaviour {
+
+	public CameraShake cameraShake;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LogoShake(){
+		cameraShake.ShakeCamera(4f, 7f, new Vector3());
+	}
+}
